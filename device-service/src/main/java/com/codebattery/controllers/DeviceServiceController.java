6c722f6eb91969974fb4bde21639397dd9dbcddc
cay914 <cay914@gmail.com>
2020-02-05 00:20:34 +0800

    去掉没有用的方法

--- device-service/src/main/java/com/codebattery/controllers/DeviceServiceController.java
+++ device-service/src/main/java/com/codebattery/controllers/DeviceServiceController.java
@@ -22,28 +22,28 @@ public class DeviceServiceController {
     private DeviceService deviceService;
     private static final Logger logger = LoggerFactory.getLogger(DeviceServiceController.class);
 
-    @RequestMapping(value="/{deviceId}",method = RequestMethod.GET)
-    public Device getDevice( @PathVariable("DeviceId") String deviceId) {
-        logger.debug("Looking up data for device {}", deviceId);
-
-        Device device = deviceService.getDevice(deviceId);
-
-        return device;
-    }
-
-    @RequestMapping(value="/{deviceId}",method = RequestMethod.PUT)
-    public void updateDevice( @PathVariable("deviceId") String deviceId, @RequestBody Device device) {
-        deviceService.updateDevice( device );
-    }
-
-    @RequestMapping(value="/{deviceId}",method = RequestMethod.POST)
-    public void saveDevice(@RequestBody Device device) {
-       deviceService.saveDevice( device );
-    }
-
-    @RequestMapping(value="/{deviceId}",method = RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteDevice( @PathVariable("deviceId") String deviceId,  @RequestBody Device device) {
-//        deviceService.deleteOrg( org );
-    }
+//    @RequestMapping(value="/{deviceId}",method = RequestMethod.GET)
+//    public Device getDevice( @PathVariable("DeviceId") String deviceId) {
+//        logger.debug("Looking up data for device {}", deviceId);
+//
+//        Device device = deviceService.getDevice(deviceId);
+//
+//        return device;
+//    }
+//
+//    @RequestMapping(value="/{deviceId}",method = RequestMethod.PUT)
+//    public void updateDevice( @PathVariable("deviceId") String deviceId, @RequestBody Device device) {
+//        deviceService.updateDevice( device );
+//    }
+//
+//    @RequestMapping(value="/{deviceId}",method = RequestMethod.POST)
+//    public void saveDevice(@RequestBody Device device) {
+//       deviceService.saveDevice( device );
+//    }
+//
+//    @RequestMapping(value="/{deviceId}",method = RequestMethod.DELETE)
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void deleteDevice( @PathVariable("deviceId") String deviceId,  @RequestBody Device device) {
+////        deviceService.deleteOrg( org );
+//    }
 }
