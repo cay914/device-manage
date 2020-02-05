85e37d4dde4ddd59753bfc8ec54543d4a4b541f3
cay914 <cay914@gmail.com>
2020-02-05 13:57:27 +0800

    添加apiModel

--- device-service/src/main/java/com/codebattery/model/Member.java
+++ device-service/src/main/java/com/codebattery/model/Member.java
@@ -7,73 +7,100 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 @Table(name = "member")
+@ApiModel(value = "成员对象", description = "成员")
 public class Member {
+	
+	@ApiModelProperty(value = "ID", required = true)
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
     
+	@ApiModelProperty(value = "用户登陆账号", required = false)
     @Column(name = "account", nullable = false)
     private String account;	//用户登陆账号
     
+	@ApiModelProperty(value = "登陆密码", required = false)
     @Column(name = "password", nullable = false)
     private String password;	//登陆密码，32位加密串	utf8
 	
+	@ApiModelProperty(value = "用户昵称")
     @Column(name = "name")
     private String  name;	//用户昵称
     
+	@ApiModelProperty(value = "手机号码")
     @Column(name = "mobile")
     private String  mobile;	//手机号码
     
+	@ApiModelProperty(value = "真实姓名")
     @Column(name = "realname")
     private String  realname;	//真实姓名
     
+	@ApiModelProperty(value = "创建时间")
     @Column(name = "create_time")
     private Date  createTime;	//创建时间
     
+	@ApiModelProperty(value = "状态")
     @Column(name = "status")
     private Integer  status;	//状态
     
+	@ApiModelProperty(value = "上次登录时间")
     @Column(name = "last_login_time")
     private Date  lastLoginTime;	//上次登录时间
     
+	@ApiModelProperty(value = "性别")
     @Column(name = "sex")
     private String sex;	//性别
     
+	@ApiModelProperty(value = "头像")
     @Column(name = "avatar")
     private String avatar;	//头像
     
+	@ApiModelProperty(value = "身份证")
     @Column(name = "idcard")
     private String idcard;	//身份证
     
+	@ApiModelProperty(value = "省")
     @Column(name = "province")
     private Long province;	//省
     
+	@ApiModelProperty(value = "市")
     @Column(name = "city")
     private Long city;	//市
     
+	@ApiModelProperty(value = "区")
     @Column(name = "area")
     private Long area;	//区
     
+	@ApiModelProperty(value = "所在地址")
     @Column(name = "address")
     private String address;	//所在地址
     
+	@ApiModelProperty(value = "备注")
     @Column(name = "description")
     private String description;	//备注
     
+	@ApiModelProperty(value = "邮箱")
     @Column(name = "email")
     private String email;	//邮箱
     
+	@ApiModelProperty(value = "编号")
     @Column(name = "code")
     private String code;	//编号
     
+	@ApiModelProperty(value = "钉钉openid")
     @Column(name = "dingtalk_openid")
     private String dingtalkOpenid;	//钉钉openid
     
+	@ApiModelProperty(value = "钉钉unionid")
     @Column(name = "dingtalk_unionid")
     private String dingtalkUnionid;	//钉钉unionid
     
+	@ApiModelProperty(value = "钉钉用户id")
     @Column(name = "dingtalk_userid")
     private String dingtalkUserid;	//钉钉用户id
 
